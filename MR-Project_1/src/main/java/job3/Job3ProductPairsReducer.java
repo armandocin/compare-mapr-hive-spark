bd249833e55e745a278295aaa3b86a9b4fdc7b4e
arcin91 <armando.cincotti@gmail.com>
2018-05-14 19:27:55 +0200

    Job3 MapReduce sorting fixed. Output checked: OK

--- MR-Project_1/src/main/java/job3/Job3ProductPairsReducer.java
+++ MR-Project_1/src/main/java/job3/Job3ProductPairsReducer.java
@@ -1,6 +1,7 @@
 package job3;
 
 import java.util.*;
+import java.util.stream.Collectors;
 import java.io.IOException;
 
 import org.apache.hadoop.io.Text;
@@ -11,13 +12,14 @@ public class Job3ProductPairsReducer extends Reducer<Text, Text, Text, Text> {
 	@Override
 	public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
 		
-		List<String> values_list = new ArrayList<>();
+		List<String> values_list = new LinkedList<>();
 		Set<String> values_set = new HashSet<>(); //no duplicates
 		values.forEach(e -> values_set.add(e.toString()));
 		values_list.addAll(values_set);
+		List<String> sorted_values = values_list.stream().sorted().collect(Collectors.toCollection(LinkedList::new));
 		
-		for(String prev : values_list) {
-			Iterator<String> it1 = values_list.listIterator(values_list.indexOf(prev)+1);
+		for(String prev : sorted_values) {
+			Iterator<String> it1 = sorted_values.listIterator(sorted_values.indexOf(prev)+1);
 			it1.forEachRemaining(curr -> {
 				String pair = "("+ prev +","+ curr +")";
 				try {
